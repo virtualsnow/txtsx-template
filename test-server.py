3a5922c1ae40c57fb785bbc6ab3fd256e1a665cf
virtualsnow <virtualsnow@pm.me>
2020-01-16 21:53:48 -0800

    organize themes better, css improvements for small screens

--- test-server.py
+++ test-server.py
@@ -6,21 +6,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def test_index():
-    return render_template('lander.html',STATIC_PREFIX='/static/')
+    return render_template('directory/lander.html',STATIC_PREFIX='/static/')
 
 @app.route('/skeleton')
 def test_skeleton():
-    return render_template('skeleton.html',STATIC_PREFIX='/static/')
+    return render_template('base/skeleton.html',STATIC_PREFIX='/static/')
 
 @app.route('/404')
 def test_404():
-    return render_template('404.html',STATIC_PREFIX='/static/')
+    return render_template('base/404.html',STATIC_PREFIX='/static/')
 
 @app.route('/contact')
 def test_contact():
-    return render_template('contact.html',STATIC_PREFIX='/static/')
+    return render_template('directory/contact.html',STATIC_PREFIX='/static/')
 
 if __name__ == "__main__":
     print("Routes:")
-    print("/\n/skeleton\n/404/\contact")
+    print("/")
+    print("/skeleton")
+    print("/404")
+    print("/contact")
     app.run()
